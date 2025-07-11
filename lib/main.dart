8de6eddbf1a3b78b9e34d1f1b8a96cf0b905af7e
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-07-11 15:38:20 +0300

    chore: running the materialApp directly

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(MaterialApp(
+  title: 'Flutter Demo',
+  theme: ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+  ),
+  home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  ),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
@@ -55,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              "You've pushed the button this many times:",
             ),
             Text(
               '$_counter',
