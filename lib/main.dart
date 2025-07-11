4cb44742f03c61338aba90b43eeaca8b0b413d3e
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-07-11 16:29:04 +0300

    feat: using the textEditingController

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
