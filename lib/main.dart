3e72b30adda35132b1ec7b3876a1a8cc9b385cc7
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-07-11 16:06:15 +0300

    feat: registration textButton

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Register"),),
+      body: TextButton(onPressed: (){}, child: const Text('Register')),
     );
   }
 }
